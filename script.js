12fa8d3aa83e996627de7bc87d2668ccdf4b255e
StallionDeveloper <164504227+RMWDeveloperKDT@users.noreply.github.com>
2025-03-28 05:42:48 +0800

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('applicationForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent form submission for validation
-
-    // Validate form fields
-    const legalName = document.getElementById('legalName').value;
-    const contactInfo = document.getElementById('contactInfo').value;
-    const leadershipRoles = document.getElementById('leadershipRoles').value;
-    const artisticSkills = document.get
